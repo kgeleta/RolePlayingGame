21165cc5ccee7556ef975a4bc6bd01af1eefca1b
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2019-12-13 07:27:54 +0100

    added walk animations

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class Movement : MonoBehaviour
+	{
+
+		private Animator _animator;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			this._animator = GetComponent<Animator>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (Input.GetButton("B"))
+			{
+				Debug.Log("Pressed B");
+			}
+			if (Input.GetButton("A"))
+			{
+				Debug.Log("Pressed A");
+			}
+			if (Input.GetButton("Y"))
+			{
+				Debug.Log("Pressed Y");
+			}
+			if (Input.GetButton("X"))
+			{
+				Debug.Log("Pressed X");
+			}
+
+			this._animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+			this._animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+
+			//			if (Input.GetAxis("Horizontal") > 0.01f)
+			//			{
+			//				Debug.Log("Right");
+			//			}
+			//			if (Input.GetAxis("Horizontal") < -0.01f)
+			//			{
+			//				Debug.Log("Left");
+			//			}
+			//			if (Input.GetAxis("Vertical") > 0.01f)
+			//			{
+			//				Debug.Log("Up");
+			//			}
+			//			if (Input.GetAxis("Vertical") < -0.01f)
+			//			{
+			//				Debug.Log("down");
+			//			}
+		}
+
+
+	}
+}
